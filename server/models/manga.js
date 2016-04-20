faea2f686f5d8cd90534f7c7109648b0252ee58c
Eduardo San Martin Morote <posva13@gmail.com>
2016-04-20 14:56:03 +0200

    Fix manga test

--- server/models/manga.js
+++ server/models/manga.js
@@ -2,6 +2,11 @@
 const mangaSchema = require('../schemas/manga.js')
 const mongoose = require('mongoose')
 
-let Manga = mongoose.model('Manga', mangaSchema)
+let Manga = null
+try {
+  Manga = mongoose.model('Manga')
+} catch (e) {
+  Manga = mongoose.model('Manga', mangaSchema)
+}
 
 module.exports = Manga
--- test/server/manga.js
+++ test/server/manga.js
@@ -2,14 +2,22 @@
 const dbURI = 'mongodb://localhost/test-mangare'
 const should = require('should')
 const mongoose = require('mongoose')
-const Manga = require('../../server/models/manga.js')
-require('mocha-mongoose')(dbURI)
+const mangaSchema = require('../../server/schemas/manga')
+const pageSchema = require('../../server/schemas/page.js')
+const clearDB = require('mocha-mongoose')(dbURI)
 
-describe('Manga', () => {
+describe('Models', () => {
   'use strict'
+  let Manga = mongoose.model('Manga', mangaSchema)
   beforeEach((done) => {
     if (mongoose.connection.db) return done()
     mongoose.connect(dbURI, done)
+  })
+
+  after((done) => {
+    mongoose.models = {}
+    mongoose.modelSchemas = {}
+    clearDB(done)
   })
 
   it('is empty on creation', (done) => {
@@ -34,6 +42,39 @@ describe('Manga', () => {
         mangas.should.have.length(1)
         done()
       })
+    })
+  })
+
+  it('throws on wrong chapter', (done) => {
+    let manga = new Manga({
+      name: 'One Piece',
+      uri: '/one-piece',
+      completed: false,
+      chapters: [{}]
+    })
+    manga.save((err, doc) => {
+      should(err).be.ok()
+      done()
+    })
+  })
+
+  it('creates chapters', (done) => {
+    let manga = new Manga({
+      name: 'test',
+      uri: '/test',
+      completed: false,
+      chapters: [
+        {
+          name: 'chapter 1',
+          uri: '/one-piece/1',
+          index: 1
+        }
+      ]
+    })
+    manga.save((err, doc) => {
+      if (err) return done(err)
+      should(doc).be.ok()
+      done()
     })
   })
 })
