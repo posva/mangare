1c69bb6c48df468ec6c874860c2f7779e4c3b712
Eduardo San Martin Morote <posva13@gmail.com>
2016-04-20 14:55:38 +0200

    Remove css

--- src/App.vue
+++ src/App.vue
@@ -36,20 +36,14 @@ export default {
 </script>
 
 <style>
-html {
-  height: 100%;
-}
-
 body {
   display: flex;
   align-items: center;
   justify-content: center;
-  height: 100%;
 }
 
 #mangare {
   color: #2c3e50;
-  margin-top: -100px;
   max-width: 600px;
   font-family: Source Sans Pro, Helvetica, sans-serif;
   text-align: center;
@@ -58,10 +52,5 @@ body {
 #mangare a {
   color: #42b983;
   text-decoration: none;
-}
-
-.logo {
-  width: 100px;
-  height: 100px
 }
 </style>
