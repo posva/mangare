7d41dcc6df2c25f6bd9b6f4bab1902a941a1e286
Eduardo San Martin Morote <posva13@gmail.com>
2016-05-13 20:10:36 +0200

    Add test for SearchBar

--- test/unit/specs/SearchBar.spec.js
+++ test/unit/specs/SearchBar.spec.js
@@ -1,15 +1,50 @@
 import Vue from 'vue'
 import SearchBar from 'src/components/SearchBar'
 
+const filled = 'filled'
+
 describe('SearchBar.vue', () => {
-  let vm
+  let vm, searchbar
   before(() => {
     vm = new Vue({
-      template: '<div><search-bar></search-bar></div>',
+      data: {
+        value: ''
+      },
+      el: 'body',
+      replace: false,
+      template: '<div><search-bar :value.sync="value" v-ref:searchbar></search-bar></div>',
       components: { SearchBar }
-    }).$mount()
+    })
+    searchbar = vm.$refs.searchbar
   })
+
   it('has an input', () => {
-    vm.$el.querySelector('input').should.exist
+    vm.$('input').should.exist
+  })
+
+  it('applies fill class when focused', (done) => {
+    searchbar.$el.should.not.have.class(filled)
+    searchbar.$('input').focus()
+    searchbar.$tick().then(() => {
+      searchbar.$el.should.have.class(filled)
+      searchbar.$('input').blur()
+      return searchbar.$tick()
+    }).then(() => {
+      searchbar.$el.should.not.have.class(filled)
+    }).then(done, done)
+  })
+
+  it('applies fill when content is not empty', (done) => {
+    searchbar.value.should.be.empty
+    vm.value = 'Something'
+    searchbar.$el.should.not.have.class(filled)
+    searchbar.$tick().then(() => {
+      searchbar.value.should.not.be.empty
+      searchbar.$el.should.have.class(filled)
+      vm.value = ''
+      return searchbar.$tick()
+    }).then(() => {
+      searchbar.$el.should.not.have.class(filled)
+    }).then(done, done)
   })
 })
