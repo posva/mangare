5c9dbb2ec1ff6d945002d13f0c9820e7d87b8436
Eduardo San Martin Morote <posva13@gmail.com>
2016-07-05 22:01:12 +0200

    Remove useless variable in dev-server

--- build/dev-server.js
+++ build/dev-server.js
@@ -11,8 +11,6 @@ module.exports = function (app, staticMiddleware) {
     webpackConfig = require('./webpack.dev.conf')
   }
 
-  // default port where dev server listens for incoming traffic
-  var port = process.env.PORT || config.dev.port
   // Define HTTP proxies to your custom API backend
   // https://github.com/chimurai/http-proxy-middleware
   var proxyTable = config.dev.proxyTable
