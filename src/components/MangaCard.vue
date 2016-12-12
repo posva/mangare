b4aaa2016332162080f4a6c048b2a9092ff53f6d
Eduardo San Martin Morote <posva13@gmail.com>
2016-12-12 14:53:40 +0100

    📦 Refactor manga card style

--- src/components/MangaCard.vue
+++ src/components/MangaCard.vue
@@ -145,170 +145,205 @@ export default {
 @import '../assets/style/palette'
 @import '../assets/style/flex'
 
-.manga-card
-  position relative
-  float left
-  overflow hidden
-  margin 1rem
-  min-width 120px
-  max-width 220px
-  height 360px
-  width 100%
-  text-align center
-  border solid 1px darken(clear, 20%)
-  border-radius 2px
-  font-weight 500
+.manga-card {
+  position: relative;
+  float: left;
+  overflow: hidden;
+  margin: 1rem;
+  min-width: 120px;
+  max-width: 220px;
+  height: 360px;
+  width: 100%;
+  text-align: center;
+  border: solid 1px darken(clear, 20%);
+  border-radius: 2px;
+  font-weight: 500;
 
-  .refreshing-banner
-    position absolute
-    top 0
-    left 0
-    z-index 2
-    width 100%
-    background-color clear
-    padding .35rem
-    font-size .75rem
-    opacity 0
-    transform translate3d(0,-100%,0)
-    transition transform 0.3s,
-               opacity 0.6s
-    &.active
-      opacity 1
-      transform translate3d(0,0,0)
+  .refreshing-banner {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 2;
+    width: 100%;
+    background-color: clear;
+    padding: .35rem;
+    font-size: .75rem;
+    opacity: 0;
+    transform: translate3d(0,-100%,0);
+    transition: transform 0.3s, opacity 0.6s
 
-  .manga-card__hover
-    backface-visibility hidden
-    position absolute
-    top auto
-    left 0
-    width 100%
-    bottom 0
-    height 100%
-    background lighten(clear, 20%)
-    color dark
-    transition transform 0.35s,
-               opacity 0.35s,
-               color 0.35s,
-               background-color 0.35s
-    transform translate3d(0,80%,0)
+    &.active {
+      opacity: 1;
+      transform: translate3d(0,0,0);
+    }
+  }
 
-    @extend .flex
-    flex-direction column
-    justify-content flex-start
+  .manga-card__hover {
+    backface-visibility: hidden;
+    position: absolute;
+    top: auto;
+    left: 0;
+    width: 100%;
+    bottom: 0;
+    height: 100%;
+    background: lighten(clear, 20%);
+    color: dark;
+    transition: transform 0.35s,
+                opacity 0.35s,
+                color 0.35s,
+                background-color 0.35s;
+    transform: translate3d(0,80%,0);
 
-  img.manga-card__image
-    position relative
-    display block
-    min-height 100%
-    max-width 100%
+    @extend .flex;
+    flex-direction: column;
+    justify-content: flex-start;
+  }
 
-  em
-    font-style normal
-    font-weight 600
-    color darken(primary, 30%)
+  img.manga-card__image {
+    position: relative;
+    min-height: 100%;
+    max-width: 100%;
+  }
 
-  .manga-card__title-container
-    padding 1em
-    height .2 * @height
-    @extend .flex
-  .manga-card__title
-    font-size 1.5rem
-    color dark
-    cursor pointer
-    text-align center
-    display inline-block
-    margin 0
+  em {
+    font-style: normal;
+    font-weight: 600;
+    color: darken(primary, 30%);
+  }
 
-  .manga-card__information
-    p
-      margin 0
-    @extend .flex
-    justify-content space-between
-    flex-direction column
-    width 100%
-    height .8 * @height
+  .manga-card__title-container {
+    padding: 1em;
+    height: .2 * @height;
+    @extend .flex;
+  }
+
+  .manga-card__title {
+    font-size: 1.5rem;
+    color: dark;
+    cursor: pointer;
+    text-align: center;
+    display: inline-block;
+    margin: 0;
+  }
+
+  .manga-card__information {
+    p {
+      margin: 0;
+    }
+
+    @extend .flex;
+    justify-content: space-between;
+    flex-direction: column;
+    width: 100%;
+    height: .8 * @height;
+
     .manga-card__information__chapters,
-    .manga-card__information__updated-at
-      font-weight 200
-    .manga-card__information__updated-at
-      font-size 1rem
-      max-height 5rem
-      padding 0 .5rem
+    .manga-card__information__updated-at {
+      font-weight: 200;
+    }
 
-    .manga-card__information__actions
-      padding 1rem
-      margin 0
-      align-self flex-end
-      list-style none
-      text-align right
+    .manga-card__information__updated-at {
+      font-size: 1rem;
+      max-height: 5rem;
+      padding: 0 .5rem;
+    }
 
-      li
-        color info
-        transition transform .25s
-        for i in (1..5)
-          &:nth-child({i})
-            transition-delay .10s + i * .05s
-        transform translate3d(0,200%,0)
-      a
-        color primary
-        position relative
-        text-decoration none
-        font-size 1.2rem
-        display inline-block
-        margin 2px 0
-        transition transform .2s
-        &:hover, &:focus
-          transform scale(.833)
+    .manga-card__information__actions {
+      padding: 1rem;
+      margin: 0;
+      align-self: flex-end;
+      list-style: none;
+      text-align: right;
 
-          &::before
-            opacity 1
-            transform scale(1.2)
-        &::before
-          position absolute
-          top -2px
-          left -7px
-          box-sizing content-box
-          padding 0 5px
-          width 100%
-          height 100%
-          border 2px solid @color
-          content ''
-          opacity 0
-          transition opacity .2s, transform .2s
-          transform scale(.833)
+      li {
+        color: info;
+        transition: transform .25s;
+        transform: translate3d(0, 200%, 0);
+        for i in (1..5) {
+          &:nth-child({i}) {
+            transition-delay: .10s + i * .05s;
+          }
+        }
+      }
 
-  // Apply these in the end
-  &:hover
-    .manga-card__hover
-      transform translate3d(0,0,0)
-      opacity .8
-      background-color dark
-      color clear
-    .manga-card__title
-      color clear
+      a {
+        color: primary;
+        position: relative;
+        text-decoration: none;
+        font-size: 1.2rem;
+        display: inline-block;
+        margin: 2px 0;
+        transition: transform .2s;
 
-    .manga-card__information__actions
-      li
-        transform translate3d(0,0,0)
+        &:hover, &:focus {
+          transform: scale(.833);
 
-  .manga-card__refresh-button
-    vertical-align middle
-    img
-      width 16px
-    outline 0
-    position relative
-    color clear
-    background none
-    border none
-    height 100%
+          &::before {
+            opacity: 1;
+            transform: scale(1.2);
+          }
+        }
 
-    &[disabled]
-      color darken(clear, 20%)
+        &::before {
+          position: absolute;
+          top: -2px;
+          left: -7px;
+          box-sizing: content-box;
+          padding: 0 5px;
+          width: 100%;
+          height: 100%;
+          border: 2px solid @color;
+          content: '';
+          opacity: 0;
+          transition: opacity .2s, transform .2s;
+          transform: scale(.833);
+        }
+      }
+    }
+  }
 
-    &:hover
-      &:not([disabled])
-        cursor pointer
-        transform scale(1.2)
+  /* Apply these in the end */
+  &:hover {
+    .manga-card__hover {
+      transform: translate3d(0,0,0);
+      opacity: .8;
+      background-color: dark;
+      color: clear;
+    }
 
+    .manga-card__title {
+      color: clear;
+    }
+
+    .manga-card__information__actions li {
+      transform: translate3d(0,0,0);
+    }
+  }
+
+  .manga-card__refresh-button {
+    vertical-align: middle;
+    outline: 0;
+    position: relative;
+    color: clear;
+    background: none;
+    border: none;
+    height: 100%;
+
+    img {
+      width: 16px;
+    }
+
+    &[disabled] {
+      color: darken(clear, 20%);
+    }
+
+    &:hover {
+      &:not([disabled]) {
+        cursor: pointer;
+        transform: scale(1.2);
+      }
+    }
+  }
+}
 </style>
 
