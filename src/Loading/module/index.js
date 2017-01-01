ef3d69ff4c5df7d76f0a613725257f92ce4ef71a
Eduardo San Martin Morote <posva13@gmail.com>
2017-01-01 18:34:36 +0100

    ✨ Add failed state to ProgressBar

--- src/Loading/module/index.js
+++ src/Loading/module/index.js
@@ -1,25 +1,31 @@
 import * as types from './types.js'
 
 const state = {
-  pendingRequests: 0
+  pendingRequests: 0,
+  failed: false
 }
 
 const mutations = {
   [types.START_REQUEST] (state) {
-    ++state.pendingRequests
+    return ++state.pendingRequests
+  },
+  [types.SET_FAILED_STATE] (state, failed) {
+    state.failed = failed
   },
   [types.END_REQUEST] (state) {
-    state.pendingRequests = Math.max(
+    return state.pendingRequests = Math.max(
       0,
       state.pendingRequests - 1
     )
   }
 }
 
-const actions = {}
+const actions = {
+}
 
 const getters = {
-  [types.PENDING_REQUESTS]: state => state.pendingRequests
+  [types.PENDING_REQUESTS]: state => state.pendingRequests,
+  [types.FAILED]: state => state.failed
 }
 
 export default {
--- src/Loading/module/types.js
+++ src/Loading/module/types.js
@@ -1,8 +1,10 @@
 // Mutations
 export const START_REQUEST = 'Loading/START_REQUEST'
 export const END_REQUEST = 'Loading/END_REQUEST'
+export const SET_FAILED_STATE = 'Loading/SET_FAILED_STATE'
 
 // Getters
 export const PENDING_REQUESTS = 'Loading/PENDING_REQUESTS'
+export const FAILED = 'Loading/FAILED'
 
 // Actions
